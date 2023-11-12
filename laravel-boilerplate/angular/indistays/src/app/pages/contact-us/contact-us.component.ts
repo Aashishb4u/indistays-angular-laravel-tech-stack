e220046f71e09cfc45e937d6bc0e466e2afea959
Aashish Bhagwat <aashishbhagwat@Aashishs-MBP.domain.name>
2023-11-12 14:45:56 +0530

    Completed Contact Us Section

--- laravel-boilerplate/angular/indistays/src/app/pages/contact-us/contact-us.component.ts
+++ laravel-boilerplate/angular/indistays/src/app/pages/contact-us/contact-us.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.scss']
 })
-export class ContactUsComponent {
-
+export class ContactUsComponent implements OnInit {
+  contactUsForm: FormGroup;
+  findings: any = [
+    'Social Media',
+    'LinkedIn',
+    'Owner'
+  ]
+  constructor(public fb: FormBuilder) {
+  }
+  ngOnInit() {
+    this.contactUsForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', Validators.required],
+      finding: ['', Validators.required],
+    });
+  }
 }
